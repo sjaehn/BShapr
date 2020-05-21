b6df338d7b74bbf1c0da0a767cb6c944d1085301
sjaehn <kontakt@jahnichen.de>
2020-05-21 19:16:11 +0200

    Add unused CCs

--- src/CCList.hpp
+++ src/CCList.hpp
@@ -24,35 +24,67 @@
         {0, "CC0 (Bank Selection)"}, \
         {1, "CC1 (Modulation)"}, \
         {2, "CC2 (Breath)"}, \
+        {3, "CC3"}, \
         {4, "CC4 (Foot)"}, \
         {5, "CC5 (Portamento Time)"}, \
         {6, "CC6 (Data Entry)"}, \
         {7, "CC7 (Main Volume)"}, \
         {8, "CC8 (Balance)"}, \
+        {9, "CC9"}, \
         {10, "CC10 (Panpot)"}, \
         {11, "CC11 (Expression)"}, \
         {12, "CC12 (Effect1)"}, \
         {13, "CC13 (Effect2)"}, \
+        {14, "CC14"}, \
+        {15, "CC15"}, \
         {16, "CC16 (General Purpose 1)"}, \
         {17, "CC17 (General Purpose 2)"}, \
         {18, "CC18 (General Purpose 3)"}, \
         {19, "CC19 (General Purpose 4)"}, \
+        {20, "CC20"}, \
+        {21, "CC21"}, \
+        {22, "CC22"}, \
+        {23, "CC23"}, \
+        {24, "CC24"}, \
+        {25, "CC25"}, \
+        {26, "CC26"}, \
+        {27, "CC27"}, \
+        {28, "CC28"}, \
+        {29, "CC29"}, \
+        {30, "CC30"}, \
+        {31, "CC31"}, \
         {32, "CC32 (Bank Selection)"}, \
         {33, "CC33 (Modulation)"}, \
         {34, "CC34 (Breath)"}, \
+        {35, "CC35"}, \
         {36, "CC36 (Foot)"}, \
         {37, "CC37 (Portamento Time)"}, \
         {38, "CC38 (Data Entry)"}, \
         {39, "CC39 (Main Volume)"}, \
         {40, "CC40 (Balance)"}, \
+        {41, "CC41"}, \
         {42, "CC42 (Panpot)"}, \
         {43, "CC43 (Expression)"}, \
         {44, "CC44 (Effect1)"}, \
         {45, "CC45 (Effect2)"}, \
+        {46, "CC46"}, \
+        {47, "CC47"}, \
         {48, "CC48 (General Purpose 1)"}, \
         {49, "CC49 (General Purpose 2)"}, \
         {50, "CC50 (General Purpose 3)"}, \
         {51, "CC51 (General Purpose 4)"}, \
+        {52, "CC52"}, \
+        {53, "CC53"}, \
+        {54, "CC54"}, \
+        {55, "CC55"}, \
+        {56, "CC56"}, \
+        {57, "CC57"}, \
+        {58, "CC58"}, \
+        {59, "CC59"}, \
+        {60, "CC60"}, \
+        {61, "CC61"}, \
+        {62, "CC62"}, \
+        {63, "CC63"}, \
         {64, "CC64 (Sustain Pedal)"}, \
         {65, "CC65 (Portamento)"}, \
         {66, "CC66 (Sostenuto)"}, \
@@ -74,6 +106,12 @@
         {82, "CC82 (General Purpose 7)"}, \
         {83, "CC83 (General Purpose 8)"}, \
         {84, "CC84 (Portamento Control)"}, \
+        {85, "CC85"}, \
+        {86, "CC86"}, \
+        {87, "CC87"}, \
+        {88, "CC88"}, \
+        {89, "CC89"}, \
+        {90, "CC90"}, \
         {91, "CC91 (E1 Reverb Depth)"}, \
         {92, "CC92 (E2 Tremolo Depth)"}, \
         {93, "CC93 (E3 Chorus Depth)"}, \
@@ -85,6 +123,24 @@
         {99, "CC99 (Non-registered Parameter Number)"}, \
         {100, "CC100 (Registered Parameter Number)"}, \
         {101, "CC101 (Registered Parameter Number)"}, \
+        {102, "CC102"}, \
+        {103, "CC103"}, \
+        {104, "CC104"}, \
+        {105, "CC105"}, \
+        {106, "CC106"}, \
+        {107, "CC107"}, \
+        {108, "CC108"}, \
+        {109, "CC109"}, \
+        {110, "CC110"}, \
+        {111, "CC111"}, \
+        {112, "CC112"}, \
+        {113, "CC113"}, \
+        {114, "CC114"}, \
+        {115, "CC115"}, \
+        {116, "CC116"}, \
+        {117, "CC117"}, \
+        {118, "CC118"}, \
+        {119, "CC119"}, \
         {120, "CC120 (All Sounds Off)"}, \
         {121, "CC121 (Reset Controllers)"}, \
         {122, "CC122 (Local Control Switch)"}, \
